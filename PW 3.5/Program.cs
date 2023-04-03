c35490ac48b6ef5fbfbaf3aa00a7a2e3ab70104f
RazinkinIlya <122820854+RazinkinIlya@users.noreply.github.com>
2023-04-03 10:39:50 +0300

    Add files via upload

---
+++ PW 3.5/Program.cs
@@ -0,0 +1,80 @@
+﻿
+SmsMessage Sms= new SmsMessage(2.5, 100, -60);
+string text = "21321312312sadasdsadsadasdasdasdasdasdasdasdasdsadasdsadsadasdsadasdsd21321312312sadasdsadsadasdasdasdasdasdasdasdasdsadasdsadsadasdsadasdsd21321312312sadasdsadsadasdasdasdasdasdasdasdasdsadasdsadsadasdsadasdsd";
+
+
+Sms.Message(text);
+
+
+
+class SmsMessage
+{
+    private double price;
+    private int maxLenght;
+    private double tax;
+    public double Price
+    {
+        get { return price; }
+        set { price = NormalizePrice(value); }
+    }
+    public int MaxLenght
+    {
+        get { return maxLenght; }
+        set { maxLenght = NormalizeMaxLenght(value); }
+    }
+
+    public double Tax
+    {
+        get { return tax; }
+        set { tax = NormalizeTaxValue(value); }
+    }
+    private double NormalizePrice(double price)
+    {
+        if (price <= 0)
+        {
+            return 1.5;
+        }
+        return price;
+    }
+    private int NormalizeMaxLenght(int maxLenght)
+    {
+        if (maxLenght <= 0)
+        {
+            return 255;
+        }
+        return maxLenght;
+    }
+    private double NormalizeTaxValue(double Tax)
+    {
+        if (Tax <= 0)
+        {
+            return 0.5;
+        }
+        return (double)Tax;
+    }
+    public  SmsMessage() { }
+    public  SmsMessage(double price, int maxLenght, double tax)
+    {
+        Price = price;
+        MaxLenght = maxLenght;
+        Tax = tax;
+
+    }
+    public void Message(string messageText)
+    {
+
+
+        if (messageText.Length < 65)
+        {
+            Console.WriteLine($"Ваше сообщение отправлено \n Текст вашего сообщения {messageText} \n Стоимость отправки сообщения {price} Рубля");
+        }
+        else
+        {
+
+            messageText = messageText.Substring(0, maxLenght);
+            Console.WriteLine(messageText.Length);
+            Console.WriteLine($"Ваше сообщение отправлено \n Текст вашего сообщения {messageText} \n Стоимость отправки сообщения {price + (messageText.Length - 65) * Tax} Рублей");
+
+        }
+    }
+}
---
+++ PW3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+SmsMessage Sms = new SmsMessage();
+string textMax = "123456789101112131415161718192012345678910111213141516171819201234567891011121314151617181920123456789101112131415161718192012345678910111213141516171819201234567891011121314151617181920" +
+    "123456789101112131415161718192012345678910111213141516171819201234567891011121314151617181920123456789101112131415161718192012345678910111213141516171819201234567891011121314151617181920";
+string textMin = "dsadsadasdasdsadasdsadsadasd";
+Sms.Message(textMin);
+
+Sms.Message(textMax);
+
+
+
+class SmsMessage
+{
+    private double price;
+    public void Message(string messageText)
+    {
+        price = 1.5;
+        if (messageText.Length < 65)
+        {
+            Console.WriteLine($"Ваше сообщение отправлено \n Текст вашего сообщения {messageText} \n Стоимость отправки сообщения {price} Рубля");
+        }
+        else { 
+            
+        messageText = messageText.Substring(0, 250);
+            Console.WriteLine(messageText.Length);
+        Console.WriteLine($"Ваше сообщение отправлено \n Текст вашего сообщения {messageText} \n Стоимость отправки сообщения {price +(messageText.Length-65)*0.5} Рубля");
+
+        }
+    }
+}
